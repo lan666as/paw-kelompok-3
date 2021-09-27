c164acdbccab53f9dd4930c7b5866f0aded7e508
lan666as <4zidan@gmail.com>
2021-09-27 22:00:28 +0700

    add schemas

---
+++ pemilu-kmteti/schemas.py
@@ -0,0 +1,19 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class SuaraBase(BaseModel):
+    nim: str
+    nama: str
+
+
+class SuaraCreate(SuaraBase):
+    pilihan: int
+
+
+class Suara(SuaraBase):
+    id_suara: int
+    pilihan: int
+
+    class Config:
+        orm_mode = True
